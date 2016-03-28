ed198832585788e460a393ac9fbda69574fcef49
unknown <HarveyC@JHC3050.jhcllp.local>
2016-03-28 22:55:19 +0100

    Latest change

--- CookBook/scripts/recipeExample.ts
+++ CookBook/scripts/recipeExample.ts
@@ -6,7 +6,7 @@ class Example implements IExample{
     name: string;
     ingredients: Ingredient[] = [];
     prepTime: string; 
-      
+              
     constructor(example: IExample) {
         this.name = example.name;
         this.ingredients = example.ingredients;
